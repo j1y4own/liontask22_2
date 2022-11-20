691c62461b2bc7410241810b4a4226a4417db08b
jiyun <missji1004@gmail.com>
2022-11-20 18:05:12 +0900

    backend

--- cashbookapp/urls.py
+++ cashbookapp/urls.py
@@ -2,6 +2,7 @@
 from django.urls import URLPattern, path
 from cashbookapp import views
 import cashbookapp.views
+import payment.views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -15,4 +16,8 @@ urlpatterns=[
     path('detail/<str:id>/',cashbookapp.views.detail, name='detail'),
     path('update_comment/<str:id>/<str:com_id>/', cashbookapp.views.update_comment, name='update_comment'),
     path('delete_comment/<str:id>/<str:com_id>/', cashbookapp.views.delete_comment, name='delete_comment'),
+    path('window', payment.views.window, name="window"),
+    path('success', payment.views.success, name="success"),
+    path('fail', payment.views.fail, name="fail"),
+    
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- cashbookprj/settings.py
+++ cashbookprj/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cashbookapp',
     'cuapp',
+    'payment',
 ]
 
 MIDDLEWARE = [
